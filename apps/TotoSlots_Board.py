ad2f493035a76f62882917c2c2ce8e9e2b0b7bcc
isunyoo <isunyoo@gmail.com>
2021-10-06 03:17:32 +0800

    Updated for changes

--- apps/TotoSlots_Board.py
+++ apps/TotoSlots_Board.py
@@ -1,5 +1,6 @@
 import json
 from web3 import Web3
+from datetime import datetime
 import utils.Random_Generator as quickPicker
 
 ganache_url = "http://localhost:8545" 
@@ -17,8 +18,8 @@ CONTRACT_ADDRESS = info_json["networks"]["4447"]["address"]
 contract = web3.eth.contract(address=CONTRACT_ADDRESS, abi=ABI, bytecode=BYTECODE)
 
 # Function to contract data in dynamic array 
-def array_pushTransact(num1, num2, num3, num4, num5, num6):  
-  tx_hash = contract.functions.array_pushData(num1, num2, num3, num4, num5, num6).transact()
+def array_pushTransact(count, num0, num1, num2, num3, num4, num5):  
+  tx_hash = contract.functions.array_pushData(count, num0, num1, num2, num3, num4, num5).transact()
   web3.eth.waitForTransactionReceipt(tx_hash)
 
 if __name__ == "__main__":
@@ -28,10 +29,15 @@ if __name__ == "__main__":
   print('New quickpick set to be stored in blockchains: ', slotslist)  
   # Trigger blockchain transaction to store in arrays
   for array in slotslist:    
-    array_pushTransact(array[0], array[1], array[2], array[3], array[4], array[5])  
+    array_pushTransact(int(slot_nums), array[0], array[1], array[2], array[3], array[4], array[5])  
   print('Total Array Length: {}'.format(contract.functions.array_getLength().call()))  
   print('All Array Data: {}'.format(contract.functions.array_popAllData().call()))
 
   # Input Array Number to retrieve
   array_num = input("Which Array Number to retrieve (Starting from 0): ")      
   print(f'Array Data[{array_num}]: ''{}'.format(contract.functions.array_getArray(int(array_num)).call()))
+  # Miscellany
+  timestamp = int(format(contract.functions.time_Call().call()))
+  print('Current Time(UTC):',datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S'))
+  print('Current Block Number: {}'.format(contract.functions.block_Call().call()))
+
--- contracts/TotoSlots.sol
+++ contracts/TotoSlots.sol
@@ -9,11 +9,15 @@ contract TotoSlots {
     uint[6][] private arr_data;    
       
     // Function to add data in dynamic array 
-    function array_pushData(uint count, uint arr1, uint arr2, uint arr3, uint arr4, uint arr5, uint arr6) public { 
+    function array_pushData(uint count, uint arr0, uint arr1, uint arr2, uint arr3, uint arr4, uint arr5) public { 
+        // Single Array to store all sub-arrays
         // arr_data = new uint[6][];
-        // arr_data.push([arr1, arr2, arr3, arr4, arr5, arr6]);
-        arr_data = new uint[6][](count);        
-        arr_data[count] = [arr1, arr2, arr3, arr4, arr5, arr6];                 
+        // arr_data.push([arr0, arr1, arr2, arr3, arr4, arr5]);        
+        uint idx;
+        arr_data = new uint[6][](count);
+        for(idx=0; idx<count; idx++) {                    
+            arr_data[idx] = [arr0, arr1, arr2, arr3, arr4, arr5];             
+        }
     }      
 
     // Function to get all data of dynamic array 
@@ -30,6 +34,16 @@ contract TotoSlots {
     // Return an array stored in the dynamic dimension will throw if index > flags.length-1 (index starts at 0)
     function array_getArray(uint index) view public returns(uint[6] memory) {
         return(arr_data[index]);
-    }    
+    }   
+     
+    // Current block timestamp is returned by now (http://www.unixtimestamp.com/)
+    function time_Call() view public returns(uint256) {        
+        return block.timestamp;
+    }
+
+    // Block Number is returned using block.number
+    function block_Call() view public returns(uint256) {
+        return block.number; 
+    }
     
 }
