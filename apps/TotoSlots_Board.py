1ea59f4237ffa07fd7cc68bb839cbc637aad1e55
isunyoo <isunyoo@gmail.com>
2021-10-06 15:55:06 +0800

    Updated for changes

--- apps/TotoSlots_Board.py
+++ apps/TotoSlots_Board.py
@@ -2,10 +2,10 @@ import json
 from web3 import Web3
 from datetime import datetime
 import utils.Random_Generator as quickPicker
+import utils.Ether_Transaction_Query as etherQuery
 
 ganache_url = "http://localhost:8545" 
 web3 = Web3(Web3.HTTPProvider(ganache_url))
-web3.eth.defaultAccount = web3.eth.accounts[0]
 
 # Opening JSON file and returns JSON object as a dictionary
 with open('../build/contracts/TotoSlots.json') as f:  
@@ -22,22 +22,69 @@ def array_pushTransact(count, num0, num1, num2, num3, num4, num5):
   tx_hash = contract.functions.array_pushData(count, num0, num1, num2, num3, num4, num5).transact()
   web3.eth.waitForTransactionReceipt(tx_hash)
 
+# Function to Retrieve Tx results historical data
+def txResultHistoryData(query_filename, start_block, end_block, principal_address):
+    # Local Lists variables 
+    _local_listLength = 0    
+    _local_From = []
+    _local_To = []
+    _local_Nonce = []
+    _local_BlockNumber = []
+    _local_Hash = []
+    _local_BlockHash = []            
+
+    # Reading from JSON file   
+    with open(etherQuery.queryEther(query_filename, start_block, end_block, principal_address), 'r') as dataContent:        
+            loaded_json = json.load(dataContent)    
+
+            for idx, key in enumerate(loaded_json):                
+                _local_From.insert(idx, loaded_json[idx]['from']) 
+                _local_To.insert(idx, loaded_json[idx]['to'])                
+                _local_Nonce.insert(idx, loaded_json[idx]['nonce'])
+                _local_BlockNumber.insert(idx, loaded_json[idx]['blockNumber'])
+                _local_Hash.insert(idx, loaded_json[idx]['hash'])
+                _local_BlockHash.insert(idx, loaded_json[idx]['blockHash'])               
+            _local_listLength = len(_local_From)
+    dataContent.close()
+
+    return _local_listLength, _local_From, _local_To, _local_Nonce, _local_BlockNumber, _local_Hash, _local_BlockHash
+
+# Function to trigger txResultHistoryData historical data results
+def queryPrincipalInput(fromBlkNum):    
+  web3.eth.defaultAccount = web3.eth.accounts[0]    
+  start_block = int(fromBlkNum)
+  end_block = int(format(contract.functions.block_Call().call()))
+  listLength, From, To, Nonce, BlockNumber, Hash, BlockHash = txResultHistoryData(web3.eth.defaultAccount, start_block, end_block, web3.eth.defaultAccount)  
+  for idx in range(listLength):
+    print('From: ',From[idx])
+    print('To: ',To[idx])
+    print('Nounce: ',Nonce[idx])
+    print('BlockNumber: ',BlockNumber[idx])
+    print('Hash: ',Hash[idx])
+    print('BlockHash: ',BlockHash[idx])
+    print('\n')  
+
+# Defining the main function
 if __name__ == "__main__":
   # Input Number of Set
   slot_nums = input("How many set to generate: ")  
   slotslist = quickPicker.toto_quickpick_generator(slot_nums)
-  print('New quickpick set to be stored in blockchains: ', slotslist)  
+  print('New quickpick set to be stored in blockchains: ', slotslist)
+
   # Trigger blockchain transaction to store in arrays
-  for array in slotslist:    
+  for array in slotslist:            
     array_pushTransact(int(slot_nums), array[0], array[1], array[2], array[3], array[4], array[5])  
   print('Total Array Length: {}'.format(contract.functions.array_getLength().call()))  
-  print('All Array Data: {}'.format(contract.functions.array_popAllData().call()))
+  print('All Array Data: {}'.format(contract.functions.array_popAllData().call()))  
 
   # Input Array Number to retrieve
   array_num = input("Which Array Number to retrieve (Starting from 0): ")      
   print(f'Array Data[{array_num}]: ''{}'.format(contract.functions.array_getArray(int(array_num)).call()))
+
   # Miscellany
   timestamp = int(format(contract.functions.time_Call().call()))
   print('Current Time(UTC):',datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S'))
   print('Current Block Number: {}'.format(contract.functions.block_Call().call()))
 
+  fromBlkNum = input("From Which Block Number to retrieve records: ")     
+  queryPrincipalInput(fromBlkNum)
